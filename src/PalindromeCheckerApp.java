5b9fe47d17a3d7b8128efa205dcc619b9a7aeb59
Narasimha-7625 <kk1085@srmist.edu.in>
2026-03-09 09:41:25 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class PalindromeCheckerApp {
+
+    // Main method - Entry point of the Java application
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("===================================");
+        System.out.println("     Welcome to Palindrome Checker ");
+        System.out.println("===================================");
+
+        // Application Details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Application Version : v1.0");
+
+        // Startup Message
+        System.out.println("Application started successfully...");
+    }
+}
